21dc1a666741a027ea6c95ce2eb75d1e81c42e13
Vladimir Nevinniy <vladimirnevinniy@Vladimirs-Mac-mini.local>
2018-02-06 17:56:34 +0200

    first commit

---
+++ workWithFiles.js
@@ -0,0 +1,31 @@
+var fs = require('fs'),
+	path = require('path');
+
+
+
+
+console.log(process.cwd());
+
+
+// var arr = ["Яблоко", "Апельсин", "Груша"];
+
+// arr.forEach(function(item, i, arr) {
+//   console.log( i + ": " + item + " (массив:" + arr + ")" );
+// });
+
+
+fs.readdir('myFolder', function(err, files){
+	if (files) {
+		files.forEach(function(file){
+
+			console.log(file)
+			var ext = path.extname(file)
+			console.log(ext)
+			var basename = path.basename(file, ext)
+			console.log(basename)
+
+		})
+	} else {
+		console.log("Catalog doesn't finde")
+	}
+})
